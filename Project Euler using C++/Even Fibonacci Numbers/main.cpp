8c0e3a0f7a7a53360a223a9a10d94103f0cd1ad3
iankristoper <iankristoper26@gmail.com>
2024-04-06 21:19:52 +0800

    Initial commit

--- Project Euler using C++/Even Fibonacci Numbers/main.cpp
+++ Project Euler using C++/Even Fibonacci Numbers/main.cpp
@@ -1,9 +1,32 @@
 #include <iostream>
 
-using namespace std;
+
+
 
 int main()
 {
-    cout << "Hello world!" << endl;
+
+    int sum = 0;
+    int previous = 1;
+    int current = 1;
+
+
+    //condition to check if the current is less than 4 million
+    while(current <= 4000000)
+    {
+        if(current % 2 == 0)
+        {
+            sum = sum + current;
+        }
+
+        int next = previous + current;  //next is sum of previous and current
+        previous = current;
+        current = next;
+
+    }
+
+    std::cout << "Sum of even-valued terms in Fibonacci sequence under 4 million: " << sum << std::endl;
+
+
     return 0;
 }
