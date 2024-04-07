9a2a50db0f9d10555745684aa3553e6a3d453bbc
iankristoper <iankristoper26@gmail.com>
2024-04-07 13:16:08 +0800

    initial commit

--- Project Euler using C++/Largest Prime Factor/main.cpp
+++ Project Euler using C++/Largest Prime Factor/main.cpp
@@ -1,9 +1,61 @@
 #include <iostream>
 
-using namespace std;
+
+
+
+
+long long int largestPrimeFactor(long long int number)
+{
+    int result = 0;
+
+    while((number % 2) == 0)
+    {
+
+        result = number / 2;
+        number = result;
+    }
+
+    for(int i = 3; i <= number; i = i + 2)
+    {
+
+        while(number % i == 0)
+        {
+            result = i;
+            number = number / i;
+        }
+    }
+
+    return result;
+
+}
+
 
 int main()
 {
-    cout << "Hello world!" << endl;
+
+    long long int number = 600851475143;
+
+    std::cout << largestPrimeFactor(number);
     return 0;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
